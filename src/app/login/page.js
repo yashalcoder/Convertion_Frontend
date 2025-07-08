380437570c351bacc4e6dbe6f6f762a17d66b470
yashalcoder <yashalrafique@gmail.com>
2025-07-08 23:30:40 +0500

    resolve nav links

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -217,7 +217,7 @@ export default function LoginPage() {
           {/* Sign Up Link */}
           <div className="text-center mt-6">
             <p className="text-gray-600">
-              Don't have an account?{" "}
+              {`Don't have an account?`}{" "}
               <Link
                 href="/signup"
                 className="text-blue-600 hover:text-blue-700 font-medium"
